e4e4bd470cdca5016392c1f3fa37cc8dc91aae65
Shivam Srivastava <er1shivam@gmail.com>
2017-08-11 19:39:23 +0530

    Finally Imlemented Rotation on fallingObject

--- Assets/scripts/spawner.cs
+++ Assets/scripts/spawner.cs
@@ -9,7 +9,7 @@ public class spawner : MonoBehaviour {
 	float nextSpawntime;
 	
 	public Vector2 spawnSizeMinMax;
-
+	public float spawnAngleMax;
 	Vector2 screenHalfsizeWorldUnits;
 	// Use this for initialization
 	void Start () {
@@ -21,10 +21,11 @@ public class spawner : MonoBehaviour {
 		if(Time.time > nextSpawntime){
 			nextSpawntime = Time.time + secondsBetweenSpawns;
 			
+			float spawnAngle = Random.Range(-spawnAngleMax,spawnAngleMax);
 			float spawnSize = Random.Range(spawnSizeMinMax.x,spawnSizeMinMax.y);
 			Vector2 spawnPosition = new Vector2 (Random.Range(-screenHalfsizeWorldUnits.x,screenHalfsizeWorldUnits.x),screenHalfsizeWorldUnits.y + spawnSize);
 			// Building new gameObject to help Scale
-			GameObject newBlock = (GameObject)Instantiate(fallingBlockprefab,spawnPosition,Quaternion.identity);
+			GameObject newBlock = (GameObject)Instantiate(fallingBlockprefab,spawnPosition,Quaternion.Euler(Vector3.forward*spawnAngle));
 			newBlock.transform.localScale = Vector2.one * spawnSize;
 			
 		}
