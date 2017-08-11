cbbbebba9a418d178ac2aa63f0b6e6e72c170a33
Shivam Srivastava <er1shivam@gmail.com>
2017-08-11 18:25:12 +0530

    Initiated and completed first step

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed = 7;
+	public float playerPositionWorld;
+	// Use this for initialization
+	void Start () {
+		float halfPlayerWidth  = transform.localScale.x / 2f;
+		playerPositionWorld = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float inputX = Input.GetAxisRaw("Horizontal");
+		float velocity = inputX * speed;
+		transform.Translate (Vector2.right*velocity* Time.deltaTime);
+
+		if(transform.position.x < -playerPositionWorld){
+			transform.position = new Vector2 (playerPositionWorld, transform.position.y);
+		}
+		if(transform.position.x > playerPositionWorld){
+			transform.position = new Vector2 (-playerPositionWorld, transform.position.y);
+		}
+
+
+	}
+}
