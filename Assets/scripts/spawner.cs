0560fd382052cb2d9d8eca590e1f78e80ee3ba8c
Shivam Srivastava <er1shivam@gmail.com>
2017-08-11 19:14:59 +0530

    Added spawning Objects falling at interval of 1s

--- Assets/scripts/spawner.cs
+++ Assets/scripts/spawner.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour {
 
+	public GameObject fallingBlockprefab;
+	public float secondsBetweenSpawns = 1;
+	float nextSpawntime;
+
+	Vector2 screenHalfsizeWorldUnits;
 	// Use this for initialization
 	void Start () {
-		
+		screenHalfsizeWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize,Camera.main.orthographicSize);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Time.time > nextSpawntime){
+			nextSpawntime = Time.time + secondsBetweenSpawns;
+			Vector2 spawnPosition = new Vector2 (Random.Range(-screenHalfsizeWorldUnits.x,screenHalfsizeWorldUnits.x),screenHalfsizeWorldUnits.y);
+			Instantiate(fallingBlockprefab,spawnPosition,Quaternion.identity);
+
+		}
 	}
 }
