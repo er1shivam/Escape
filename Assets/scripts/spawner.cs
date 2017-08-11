f59f2ff298adbeafddfdf9f0ca05b36406069fe8
Shivam Srivastava <er1shivam@gmail.com>
2017-08-11 19:20:12 +0530

    Added Diff size of falligBlocks

--- Assets/scripts/spawner.cs
+++ Assets/scripts/spawner.cs
@@ -7,6 +7,8 @@ public class spawner : MonoBehaviour {
 	public GameObject fallingBlockprefab;
 	public float secondsBetweenSpawns = 1;
 	float nextSpawntime;
+	
+	public Vector2 spawnSizeMinMax;
 
 	Vector2 screenHalfsizeWorldUnits;
 	// Use this for initialization
@@ -18,7 +20,9 @@ public class spawner : MonoBehaviour {
 	void Update () {
 		if(Time.time > nextSpawntime){
 			nextSpawntime = Time.time + secondsBetweenSpawns;
-			Vector2 spawnPosition = new Vector2 (Random.Range(-screenHalfsizeWorldUnits.x,screenHalfsizeWorldUnits.x),screenHalfsizeWorldUnits.y);
+			
+			float spawnSize = Random.Range(spawnSizeMinMax.x,spawnSizeMinMax.y);
+			Vector2 spawnPosition = new Vector2 (Random.Range(-screenHalfsizeWorldUnits.x,screenHalfsizeWorldUnits.x),screenHalfsizeWorldUnits.y + spawnSize/2f);
 			Instantiate(fallingBlockprefab,spawnPosition,Quaternion.identity);
 
 		}
